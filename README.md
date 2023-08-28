f98b04c7afc26712e636390363f790fea9ea11b4
Pranathi235 <143433992+Pranathi235@users.noreply.github.com>
2023-08-28 22:18:23 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-practice-start-uz5npi
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-practice-start-h29taj)
---
+++ app/app.component.ts
@@ -0,0 +1,18 @@
+import {Component} from '@angular/core';
+import {PassengerI} from './passenger';
+
+@Component({
+  selector:'app-root',
+  templateUrl:'./app.component.html',
+  styleUrls:['app.component.css']
+})
+export class AppComponent {
+
+ passengers: PassengerI[] = [{
+
+ }
+   
+ ]
+ 
+ 
+  }
